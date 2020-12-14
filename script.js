917305e7ffd8bd8c7970fd4bbe3a7a25a3135e1b
bardanovk <bardanovk@gmail.com>
2020-12-14 22:32:44 +0300

    Верстка страницы, подготовка к заполнению

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    document.querySelector('body').style = "display: block"
+}
+
+function show(id) {
+    id.style = "display: block"
+}
+
+function hide(id) {
+    id.style = "display: none"
+}
